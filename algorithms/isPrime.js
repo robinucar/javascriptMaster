1837ab34f13ee3f3c289683c15b70985c6b11440
Robin <mehmet.ucar@imediapro.co.uk>
2020-07-23 22:48:06 +0100

    is prime faster way and time complexity

--- algorithms/isPrime.js
+++ algorithms/isPrime.js
@@ -10,3 +10,26 @@ const isPrime = (num) => {
 console.log(isPrime(5));
 console.log(isPrime(9));
 console.log(isPrime(11));
+
+
+
+//faster way
+
+const isPrime1 = (num) => {
+  for (let i = 2; i < Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPrime1(5));
+console.log(isPrime1(9));
+console.log(isPrime1(11));
+
+
+// Time Complexity :
+//Best case : O(1)  if number is 1 or 2
+//Avarage case : O(n) (sfaster way : O(sqrt(n)))
+//Worst Case : O(n) if number is big prime number such as 27,277
