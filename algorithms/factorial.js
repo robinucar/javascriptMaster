79e882222496751e489ee8dedcc744a232e0671d
Robin <mehmet.ucar@imediapro.co.uk>
2020-07-25 22:30:56 +0100

    factorial time complexity

--- algorithms/factorial.js
+++ algorithms/factorial.js
@@ -9,3 +9,5 @@ const factorial = number => {
 console.log(factorial(4)); // 4*3*2*1 = 24
 console.log(factorial(5)); // 5*4*3*2*1 = 120
 console.log(factorial(6)); // 6*5*4*3*2*1 = 720
+
+// Time Complexity = O(n) = Linear
