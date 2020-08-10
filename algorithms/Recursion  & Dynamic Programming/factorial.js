e5192ef0307f70c778f82b52664d8fd587629c10
Robin <mehmet.ucar@imediapro.co.uk>
2020-08-10 13:52:55 +0100

    faactorial algoritm and its time complexity (recursion way)

--- algorithms/Recursion  & Dynamic Programming/factorial.js
+++ algorithms/Recursion  & Dynamic Programming/factorial.js
@@ -1,8 +1,9 @@
+//faactorial algorithm recursion way
 const factorial = number => {
   if(number === 1) {
     return 1;
   }
-  return number * factorial(number - 1)
+  return number * factorial (number - 1)
 }
 
 
@@ -10,3 +11,5 @@ const factorial = number => {
 console.log(factorial(4)); // 4*3*2*1 = 24
 console.log(factorial(5)); // 5*4*3*2*1 = 120
 console.log(factorial(6)); // 6*5*4*3*2*1 = 720
+
+// Time Complexity = O(n) = Linear
