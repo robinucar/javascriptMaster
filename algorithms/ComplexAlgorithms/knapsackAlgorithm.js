1b5f31e0818151dd1e669671c496ad664186919b
Robin <mehmet.ucar@imediapro.co.uk>
2020-09-05 15:58:55 +0100

    improved knapsack algorithm

--- algorithms/ComplexAlgorithms/knapsackAlgorithm.js
+++ algorithms/ComplexAlgorithms/knapsackAlgorithm.js
@@ -1,35 +1,33 @@
 const knapsack = (items, cap, itemIndex) => {
-  if (cap === 0 || itemIndex < 0) {
-    return {
-      items: [],
-      value: 0,
-      weight: 0,
-    };
-  }
-  if (cap < items[itemIndex].weight) {
-    return knapsack(items, cap, itemIndex - 1);
-  }
-  const sackWithItem = knapsack(
-    items,
-    cap - items[itemIndex].weight,
-    itemIndex - 1
-  );
+    if (cap === 0 || itemIndex < 0) {
+      return {
+        items: [], value: 0, weight: 0
+      };
+    }
+    if (cap < items[itemIndex].weight) {
+      return knapsack(items, cap, itemIndex - 1);
+    }
+    const sackWithItem = knapsack(
+      items,
+      cap - items[itemIndex].weight,
+      itemIndex - 1
+    );
 
-  const sackWithoutItem = knapsack(items, cap, itemIndex - 1);
+    const sackWithoutItem = knapsack(items, cap, itemIndex - 1 );
 
-  const valueWithItem = sackWithItem.value + items[itemIndex].value;
-  const valueWithoutItem = sackWithoutItem.value;
+    const valueWithItem = sackWithItem.value + items[itemIndex].value;
+    const valueWithoutItem = sackWithoutItem.value;
 
-  if (valueWithItem > valueWithoutItem) {
-    const updatedSack = {
-      items: sackWithItem.items.concat(items[itemIndex]),
-      value: sackWithItem.value + items[itemIndex].value,
-      weight: sackWithItem.weight + items[itemIndex].weight,
-    };
-    return updatedSack;
-  } else {
-    return sackWithoutItem;
-  }
+    if(valueWithItem > valueWithoutItem) {
+      const updatedSack = {
+        items: sackWithItem.items.concat(items[itemIndex]),
+        value: sackWithItem.value + items[itemIndex].value,
+        weight: sackWithItem.weight + items[itemIndex].weight
+      };
+      return updatedSack;
+    } else {
+      return sackWithoutItem
+    }
 };
 
 const items = [
@@ -42,3 +40,5 @@ const maxCap = 8;
 
 const sack = knapsack(items, maxCap, items.length - 1);
 console.log(sack);
+
+//Time Complexity = O(n^2)
